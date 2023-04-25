77af4d32a98fd57236956d067b1ebf246ac16476
Eric Viera <srthompsoncode@gmail.com>
2023-04-25 14:52:44 -0300

    feat: add script to update dataset (#3)
    * feat: add script to update dataset
    
    * feat: add github action job
    
    * feat: update dataset dates
    
    * feat: change cron config
    
    * chore: add pull request creation action
    
    * fix: dataset reference

---
+++ bump-dates.js
@@ -0,0 +1,16 @@
+'use strict'
+const  fs = require('fs')
+const dataSet = JSON.parse(fs.readFileSync("./dataset.json", "utf8"));
+
+dataSet.map(item => {
+    const hour = Math.round(Math.random() * 24)
+    const min = Math.round(Math.random() * 60) 
+    const date = new Date(item.date)
+
+    date.setMonth(date.getMonth() + 6);
+    date.setHours(hour, min)
+    item.date = date.toISOString()
+
+})
+
+fs.writeFileSync("./dataset.json", JSON.stringify(dataSet, null, 2));
